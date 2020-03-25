e3e719e48e54e8181ce3b7681a913d0388508f2b
StephArn <34690734+StephArn@users.noreply.github.com>
2020-03-26 00:46:40 +0200

    Update $nfa_accepter.py

--- $nfa_accepter.py
+++ $nfa_accepter.py
@@ -1,25 +1,38 @@
 def evaluate(word, stare_curenta):
-
-    global stari_fin, tranz
-
+    print(word)
+    global stari_fin, tranz, ok
     if word=="":
         if stare_curenta in stari_fin:
+            ok=1
             return 1
         else:
+            ok=-1
             return 0
 
-    urm=[] # retine starea/ starile care pot urma imediat in fct de stare curenta si de litera curenta
+    urm=[]# retine starea/ starile care pot urma imediat in fct de stare curenta si de litera curenta
     cur=word[0]  # litera curenta din cuvantul de verificat
+    lambd=0
 
     for t in tranz[stare_curenta]:
         if t[0]==cur:
-            urm.append(t[0])
+            urm.append(t[1])
+        elif t[0]=='$':
+            lambd =1
+            stare_curenta = t[1]
+            urm.append(t[1])
 
-    if not t:
+
+    if not urm:
+        ok=-1
         return 0
-
     for stare in urm:
-        evaluate(word[1:], stare)
+        print(stare)
+        print("aici")
+        if lambd==0:
+            evaluate(word[1:], stare)
+        if lambd==1:
+            evaluate(word, stare)
+
 
 
 
@@ -31,14 +44,14 @@ m = int(f.readline())  # numar caractere alfabet
 
 alfabet = []
 
-for i in f.readline().split():
+for i in f.readline().strip().split():
     alfabet.append(i)
+
+stari_fin = []
 
 q0 = int(f.readline())  # stare initiala
 
 k = int(f.readline())  # numar stari finale
-
-stari_fin = []
 
 for i in f.readline().split():
     stari_fin.append(int(i))
@@ -53,11 +66,14 @@ for i in range(l):
 
 word = input("testing the word: ")
 
-ok=evaluate(word, q0)
+ok=0
 
-if ok:
+okret= evaluate(word, q0)
+
+if ok==1:
     print("DA")
-else:
+if ok==-1:
     print("NU")
+
 
 f.close()
